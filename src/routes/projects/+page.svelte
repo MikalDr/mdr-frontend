a169fb835925723846f62a5b360156858556f640
mikaldr <mikal-dr@hotmail.com>
2024-02-25 15:40:32 +0100

    Fixed the mock data

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,11 @@
 <script>
     const projects = [
-        {"title": "Test",
-        "content": "This is a test project. I would like this to look neat. and not bad"},
-        {"title": "Test",
-        "content": "This is a test project. I would like this to look neat. and not bad"},
-        {"title": "Test",
-        "content": "This is a test project. I would like this to look neat. and not bad"},
+        {"title": "FeedApp",
+        "content": "This is a project created in relation to the subject Dat250 at HVL. It is a voting application where user can create polls on different subjects"},
+        {"title": "TryBlocking - Rust",
+        "content": "TryBlocking is a game I created during a gamejam. This is a project centered around recreating it in Bevy, a game engine for Rust."},
+        {"title": "Battle-System",
+        "content": "Battle-system is a haskell project created in relation to the subject INF221 at UiB. The project is essentially creating a pokemon battle system in Haskell"},
     ]
 </script>
 
@@ -17,6 +17,7 @@
                 <div class="articles">
                     <h3>{p.title}</h3>
                     <p>{p.content}</p>
+                    <div><a href="">read more</a> <a href="">github</a></div>
                 </div>
             {/each}
         </div>

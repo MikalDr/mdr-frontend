104bed92117d09d5f74438935ceab106d356f619
MikalDr <mikal-dr@hotmail.com>
2024-03-18 19:29:35 +0100

    Did some changes to layout

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,7 +1,44 @@
 <div class="mainbox">
-    <div>
-        <img src="https://avatars.githubusercontent.com/u/105053235?v=4" alt="githubIcon" class="responsive-image">
-
-        <h1>How to contact me</h1>
+    <h1>How to contact me</h1>
+    <div class="contactbox">
+        <img src="https://avatars.githubusercontent.com/u/105053235?v=4" alt="githubIcon" class="responsive-image-small">
+        <div class="infobox">
+            <h2>GitHub</h2>
+            <a href="https://github.com/MikalDr"><p>MikalDr</p></a>
+        </div>
+    </div>
+    <div class="contactbox">
+        <img src="https://i.pinimg.com/originals/84/6a/03/846a03c6760f83f3a0e92609bca9cebf.jpg" alt="githubIcon" class="responsive-image-small">
+        <div class="infobox">
+            <h2>Email</h2>
+            <p>mikal-dr@hotmail.com</p>
+        </div>
     </div>
 </div>
+
+<style>
+    .mainbox {
+        display: flex;
+        flex-direction: column;
+        margin: 2rem;
+        align-items: center;
+        gap: 1rem;
+        }
+    .contactbox {
+        display: flex;
+        background-color: rgb(70, 22, 16);
+        width: 90%;
+        padding: 1rem;
+        border-radius: 2rem;
+        gap: 2rem;
+    }
+    .responsive-image-small {
+        max-width: 5rem;
+        min-width: 150px;
+        height: auto;
+        border-radius: 100%;
+    }
+    .infobox {
+
+    }
+</style>
